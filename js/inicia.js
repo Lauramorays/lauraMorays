47e33a5a51a5d55ba870cc13d512cc4741e56c11
Lauramorays <82046652+Lauramorays@users.noreply.github.com>
2022-12-13 14:41:14 -0300

    Add files via upload

---
+++ js/inicia.js
@@ -0,0 +1,20 @@
+
+      
+  $(document).ready(function(){
+    $('.sidenav').sidenav();
+  });
+
+
+function newFunction() {
+    $(document).ready(function () {
+        $('.carousel').carousel();
+    });
+}
+
+$(document).ready(function(){
+    $('.sidenav').sidenav();
+  });
+
+  $(document).ready(function(){
+    $('.carousel').carousel();
+  });
